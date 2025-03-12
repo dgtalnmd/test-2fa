6aba407eda2efa246a0c237bfb056cbaa3ed3c1a
Иванов Михаил Викторович <ivanov_mv@open.ru>
2025-03-13 00:13:51 +0500

    add possibility to set API_URL variable from outside

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/api/v1/front-office/`,
+    baseUrl: `${import.meta.env.API_URL}/api/v1/front-office/`,
   }),
   endpoints: () => ({}),
 });
